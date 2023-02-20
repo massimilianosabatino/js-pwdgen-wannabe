46dae0d232a7b6b69ff7d73d43e51394c0529bbf
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-02-20 17:13:15 +0100

    generate password

--- js/main.js
+++ js/main.js
@@ -4,4 +4,9 @@ const userSurname = prompt("Qual'è il tuo cognome?");
 let userColor = prompt("Qual'è il tuo colore preferito?")
 const passwordClassNumber = 89;
 
+// Concatena le risposte in una nuova variabile
+let passwordGenerated = `${userName}${userSurname}${userColor}${passwordClassNumber}`;
+
+// Output in console
+console.log(passwordGenerated);
 
