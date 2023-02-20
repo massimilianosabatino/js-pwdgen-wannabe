0877471a1e6da85bc648e6978517070b002bf93e
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-02-20 17:08:32 +0100

    variables definition

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Definizione nome variabili
+const userName = prompt("Qual'è il tuo nome?");
+const userSurname = prompt("Qual'è il tuo cognome?");
+let userColor = prompt("Qual'è il tuo colore preferito?")
+const passwordClassNumber = 89;
+
+
